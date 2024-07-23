d0462f25d21ed9bddf1903da56835ddebe622337
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-07-22 20:07:51 -0400

    updated javascript code to include gameplay , play maker and winner logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,42 +1,62 @@
-console.log('Tic Tac Toe');
+console.log("Tic Tac Toe");
+const prompt = require("prompt-sync")();
 
 //state of the game
 
 let turn, board, winner;
-
+const winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 /* ------- Game Logic -------*/
-init()
+init();
+gamePlay();
 
 function init() {
-    turn = 'X';
-    board = [
-       ' ' ,' ' ,' ',
-       ' ' ,' ' ,' ',
-       ' ' ,' ' ,' '
-    ];
-    winner = null;
-    render()
+  turn = "X";
+  board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+  winner = null;
+  render();
 }
 
-function render(){
-    console.log(`It is player ${turn}'s turn`)
-    renderBoard();
+function render() {
+  console.log(`It is player ${turn}'s turn`);
+  renderBoard();
 }
 
 function renderBoard() {
-    console.log(`  ${board[0]}|${board[1]}|${board[2]}`);
-    console.log(' --|-|--');
-    console.log(`  ${board[3]}|${board[4]}|${board[5]}`);
-    console.log(' --|-|--');
-    console.log(`  ${board[6]}|${board[7]}|${board[8]}`);
-
+  console.log(`  ${board[0]}|${board[1]}|${board[2]}`);
+  console.log(" --|-|--");
+  console.log(`  ${board[3]}|${board[4]}|${board[5]}`);
+  console.log(" --|-|--");
+  console.log(`  ${board[6]}|${board[7]}|${board[8]}`);
 }
 
-function gamePlay() {
-    while(!winner){
-        console.log(`Select `)
+function playMaker(playerMove) {
+  if (board[playerMove] === "X" || board[playerMove] === "O")
+    console.log("That space is taken, please select another.");
+  else if (playerMove > 7 || playerMove < 0 )
+    console.log("Invlaid move, please select another.");
+  else {
+    board[playerMove] = turn;
+    turn === "X" ? (turn = "O") : (turn = "X");
+  }
+}
+
+function winnerWinnerChickenDinner(){
+    for (const combo of winningCombos){
+         if(board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]){
+            winner = board[combo[0]]
+         }
     }
 }
 
-
+function gamePlay() {
+  while (!winner) {
+    const playerMove = prompt(
+      `${turn} It's your move, please select a space : `
+    ).toUpperCase();
+    if (playerMove === "EXIT") break;
+    playMaker(playerMove);
+    winnerWinnerChickenDinner();
+    render();
+  }
+}
